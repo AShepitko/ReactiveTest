64f8cff485c2de4e14c30f515f42e9d6a0efe19a
Alexei Shepitko <ashepitko@ruswizards.com>
2017-01-25 15:30:33 +0300

    add classes for Main screen

---
+++ ReactiveTest/MainScreen/Model/Series.swift
@@ -0,0 +1,13 @@
+//
+//  Series.swift
+//  ReactiveTest
+//
+//  Created by Alexei Shepitko on 25/01/2017.
+//  Copyright © 2017 Distillery. All rights reserved.
+//
+
+import CoreData
+
+class Series: NSManagedObject {
+
+}
