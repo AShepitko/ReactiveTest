7ed22829ba156a12d81cb37f031b92410d02942d
Alexei Shepitko <ashepitko@ruswizards.com>
2017-01-26 14:49:10 +0300

    fetch and output series data using reactive

--- ReactiveTest/MainScreen/View/MainViewController.swift
+++ ReactiveTest/MainScreen/View/MainViewController.swift
@@ -7,22 +7,50 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var serverIDLabel: UILabel!
+    @IBOutlet weak var urlLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageUrlLabel: UILabel!
+    @IBOutlet weak var genresLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var seasonsCountLabel: UILabel!
+    
     let viewModel = MainViewModel()
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = UIColor.lightGray
+
+        viewModel.series.asObservable().subscribe(onNext: { series in
+            if let series = series {
+                DispatchQueue.main.async {
+                    self.serverIDLabel.text = "ID:\n\(series.serverID.description)"
+                    self.urlLabel.text = "URL:\n\(series.url)"
+                    self.nameLabel.text = "Name:\n\(series.name)"
+                    self.imageUrlLabel.text = "Image:\n\(series.imageUrl)"
+                    self.genresLabel.text = "Genres:\n\(series.genres)"
+                    self.summaryLabel.text = "Summary:\n\(series.summary)"
+                    self.seasonsCountLabel.text = "Seasons:\n\(series.seasons?.count.description)"
+                }
+            }
+        })
+        .addDisposableTo(disposeBag)
         
-        viewModel.test()
+        viewModel.fetchData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+        viewModel.fetchData()
     }
     
 
--- ReactiveTest/MainScreen/ViewModel/MainViewModel.swift
+++ ReactiveTest/MainScreen/ViewModel/MainViewModel.swift
@@ -8,19 +8,55 @@
 
 import Foundation
 import SwiftyJSON
+import RxSwift
+import CoreData
 
 class MainViewModel {
 
-    func test() {
-        let configuration = URLSessionConfiguration.default
-        let session = URLSession(configuration: configuration)
-        session.dataTask(with: URL(string: "https://dl.dropboxusercontent.com/u/65496631/got.json")!) { (data, response, error) in
-            if let data = data {
-                let json = JSON(data: data)
-                print("JSON:\n\(json)")
+    var series: Variable<Series?> = Variable(nil)
+    
+    func fetchData() {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            var requestResult:[Series] = []
+            let fetchRequest = Series.fetchRequest() as NSFetchRequest<Series>
+            do {
+                requestResult = try appDelegate.persistentContainer.viewContext.fetch(fetchRequest)
+                if requestResult.count > 0 {
+                    self.series.value = requestResult.first
+                }
+                
+                let configuration = URLSessionConfiguration.default
+                let session = URLSession(configuration: configuration)
+                session.dataTask(with: URL(string: "https://dl.dropboxusercontent.com/u/65496631/got.json")!) { (data, response, error) in
+                    if let data = data {
+                        let json = JSON(data: data)
+                        let serverID = json["id"].int64Value
+                        let url = json["url"].stringValue
+                        let name = json["name"].stringValue
+                        let imageUrl = json["image"]["original"].stringValue
+                        let summary = json["summary"].stringValue
+                        if let existingSeries = requestResult.first(where: { $0.serverID == serverID }) {
+                            self.series.value = existingSeries
+                        }
+                        else {
+                            self.series.value = NSEntityDescription.insertNewObject(forEntityName: "Series", into: appDelegate.persistentContainer.viewContext) as? Series
+                        }
+                        self.series.value?.serverID = serverID
+                        self.series.value?.url = url
+                        self.series.value?.name = name
+                        self.series.value?.imageUrl = imageUrl
+                        self.series.value?.summary = summary
+                        appDelegate.saveContext()
+                    }
+                }
+                .resume()
+                
+            }
+            catch let error as NSError {
+                print("Fetch error: \(error)")
             }
         }
-        .resume()
+
     }
     
 }
